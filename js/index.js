151ac562d0c4d39952d938eca8e91f9fc0bdcc28
gopika <gopikaksurendran@gmail.com>
2024-08-20 13:32:51 +0530

    Added

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function addDetails(){
+    let info1,info2,info3,info4,info5;
+    info1=document.getElementById("input1").value
+    info2=document.getElementById("input2").value
+    info3=document.getElementById("input3").value
+    info4=document.getElementById("input4").value
+    info5=document.getElementById("input5").value
+
+}
